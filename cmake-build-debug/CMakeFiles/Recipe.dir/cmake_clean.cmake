c1662371c5f739c781786d0109d339f62aeb6f9e
Raven <1772133019@qq.com>
2022-10-31 12:10:58 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Recipe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Recipe.dir/main.cpp.o"
+  "Recipe"
+  "Recipe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Recipe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ recipe.h
@@ -0,0 +1,11 @@
+//
+// Created by Ravin on 2022/10/31.
+//
+
+#ifndef RECIPE_RECIPE_H
+#define RECIPE_RECIPE_H
+
+
+
+
+#endif //RECIPE_RECIPE_H
